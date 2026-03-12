d3294f5296d12ad38e0751ba3ac4866115a8161d
jhon <jhondennivistal@gmail.com>
2026-03-12 15:43:34 +0800

    Updated jwks endpoint

--- app/Http/Controllers/JwksController.php
+++ app/Http/Controllers/JwksController.php
@@ -8,10 +8,9 @@ class JwksController extends BaseController
 {
     public function jwks(): JsonResponse
     {
-        $publicKeyPath = storage_path('jwt/public_rsa.pem');
-        $publicKeyPem  = file_get_contents($publicKeyPath);
-        $publicKeyRes  = openssl_pkey_get_public($publicKeyPem);
-        $keyDetails    = openssl_pkey_get_details($publicKeyRes);
+        $publicKeyPem = config('jwt.keys.public');
+        $publicKeyRes = openssl_pkey_get_public($publicKeyPem);
+        $keyDetails   = openssl_pkey_get_details($publicKeyRes);
 
         $n = rtrim(strtr(base64_encode($keyDetails['rsa']['n']), '+/', '-_'), '=');
         $e = rtrim(strtr(base64_encode($keyDetails['rsa']['e']), '+/', '-_'), '=');
